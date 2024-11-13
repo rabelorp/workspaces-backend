8ce06cc7def6db871fd87b2be42b8a398230b2c2
Robson Rabelo <rabelo_rp@hotmail.com>
2024-11-13 16:27:00 -0300

    build: homolog

--- src/auth-rabelodigital/auth-rabelodigital.controller.ts
+++ src/auth-rabelodigital/auth-rabelodigital.controller.ts
@@ -40,6 +40,7 @@ export class AuthRabelodigitalController {
         await this.authRabelodigitalService.getProfile(loginDto);
       return this.authService.validateSocialLogin('rabelodigital', socialData);
     } catch (error) {
+      console.log({error, request: error?.request, response: error?.response})
       if (error.response && error.response.status === 401) {
         throw new HttpException('Unauthorized access', HttpStatus.UNAUTHORIZED);
       }
